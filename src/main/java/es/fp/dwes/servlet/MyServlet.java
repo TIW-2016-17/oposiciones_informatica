f4ed07b9e4cdb9cdd0d32eacc05410822f75a064
Tecnologias de Internet para la web <tiw@vagrant.vm>
2020-05-18 16:54:35 +0000

    Ejercicio_9_servlets

--- src/main/java/es/fp/dwes/servlet/MyServlet.java
+++ src/main/java/es/fp/dwes/servlet/MyServlet.java
@@ -1,39 +1,52 @@
 package es.fp.dwes.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;       
+	private static final long serialVersionUID = 1L;
 	
-    
-    public MyServlet() {
-        super();
-    }
-
-	
-    public void init(ServletConfig config) throws ServletException {
-		
-    }
-
-	
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    		throws ServletException, IOException {
-	
-	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-	
+		
+		// leemos los parámetros de la petición
 
-			   
+		String nombre = (String) request.getParameter("name");
+		String correo = (String) request.getParameter("email");
+
+		// fijar el tipo de contenido
+    	response.setContentType("text/html;charset=UTF-8");
+
+		// Obtener el writer
+		PrintWriter out = response.getWriter();
+
+		// respuesta del Write
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Form fields</title>");
+		out.println("</head>");
+		out.println("<body>");
+
+		out.println("Client information ");
+		out.println("<h2>Name: " + nombre + "</h2>");
+		out.println("<h2>Email: " + correo + "</h2>");
+		out.println("</body>");
+		out.println("</html>");
+
+		out.flush();
+		out.close();
 	}
+	
 
 }
+
+
