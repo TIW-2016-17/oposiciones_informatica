a2f83b40e87ef59e124401cc22b4927a443bdac4
Tecnologias de Internet para la web <tiw@vagrant.vm>
2020-05-18 15:00:31 +0000

    Ejercicios extra servlets

---
+++ src/main/java/es/fp/dwes/servlet/MyServlet.java
@@ -0,0 +1,39 @@
+package es.fp.dwes.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;       
+	
+    
+    public MyServlet() {
+        super();
+    }
+
+	
+    public void init(ServletConfig config) throws ServletException {
+		
+    }
+
+	
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+	
+
+			   
+	}
+
+}
